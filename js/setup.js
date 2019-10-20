fd520bcf9644d1b6fd569d0e207814b6c56b48a2
Alexey197 <margadonnn3@gmail.com>
2019-10-20 19:29:14 +0300

    backend

--- js/setup.js
+++ js/setup.js
@@ -12,28 +12,23 @@
   var getWizard = function (creatures) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = creatures.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = creatures.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = creatures.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = creatures.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = creatures.colorEyes;
     return wizardElement;
   };
 
-  // Армия клонов())
-
-  var getWizards = function (creatures) {
+  var successHandler = function (creatures) {
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < creatures.length; i++) {
-      fragment.appendChild(getWizard(creatures[i]));
+    for (var i = 0; i < WIZARDS_QUANTITY; i++) {
+      fragment.appendChild(getWizard(window.util.getRandomArrElement(creatures)));
     }
-    return fragment;
+    similarListElement.appendChild(fragment);
   };
 
-  // Инициализация
-
-  var initApp = function () {
-    similarListElement.appendChild(getWizards(window.data.getCreatures(WIZARDS_QUANTITY)));
+  var showWizards = function () {
+    window.backend.load(successHandler, window.message.getError);
     document.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  initApp();
+  showWizards();
 })();
-
